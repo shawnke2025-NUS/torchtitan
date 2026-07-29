e42f59b54444bf7fc3a1b762565d8798fbf16f72
shawnke2025-NUS <shawnke2025@gmail.com>
2026-07-29 11:46:42 +0800

    Update model.py

--- torchtitan/models/qwen3_5/model.py
+++ torchtitan/models/qwen3_5/model.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-#version1：2026.7.29 10:29
+#version1：2026.7.29 9：57
 
 from dataclasses import dataclass
 from typing import Literal
@@ -1020,5 +1020,3 @@ class Qwen35Model(Decoder):
             return out
             
         return x
-
-
